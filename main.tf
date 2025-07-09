0c897992a5ce44ab2cab2c0e3cc49bb864d318de
Mohammad Hossein Nemati <ckoliber@gmail.com>
2025-07-09 15:35:21 +0330

    feat: add nonsensitive to show local-exec output

--- main.tf
+++ main.tf
@@ -2,3 +2,42 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {}
 }
+
+resource "terraform_data" "this" {
+  triggers_replace = merge(var.triggers, {
+    hosts              = var.hosts
+    groups             = var.groups
+    create_playbook    = var.create_playbook
+    create_extra_args  = var.create_extra_args
+    destroy_playbook   = var.destroy_playbook
+    destroy_extra_args = var.destroy_extra_args
+  })
+
+  provisioner "local-exec" {
+    when    = create
+    quiet   = true
+    command = "echo $SCRIPT | base64 -d | bash"
+    environment = {
+      SCRIPT = nonsensitive(base64encode(templatefile("${path.module}/templates/playbook.sh", {
+        hosts      = self.triggers_replace.hosts
+        groups     = self.triggers_replace.groups
+        playbook   = self.triggers_replace.create_playbook
+        extra_args = self.triggers_replace.create_extra_args
+      })))
+    }
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    quiet   = true
+    command = "echo $SCRIPT | base64 -d | bash"
+    environment = {
+      SCRIPT = nonsensitive(base64encode(templatefile("${path.module}/templates/playbook.sh", {
+        hosts      = self.triggers_replace.hosts
+        groups     = self.triggers_replace.groups
+        playbook   = self.triggers_replace.destroy_playbook
+        extra_args = self.triggers_replace.destroy_extra_args
+      })))
+    }
+  }
+}
